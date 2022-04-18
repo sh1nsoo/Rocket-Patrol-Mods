5f5776e3d7f9c55aaca6575be8f47b59526eb882
shinsoo <elgkim@ucsc.edu>
2022-04-17 20:10:19 -0700

    Update Play.js

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -14,11 +14,6 @@ class Play extends Phaser.Scene {
     create() {
         this.clouds = this.add.tileSprite(0,0, 640, 480, 'clouds').setOrigin(0,0);
         this.belt = this.add.tileSprite(0,0, 640, 480, 'conveyer_belt').setOrigin(0,0);
-        //this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize *2, 0x00FF00).setOrigin(0,0);
-        //this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0,0);
-        //this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0,0);
-        //this.add.rectangle(0,0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
-        //this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
 
         //this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
         if (game.settings.mode == true) {
@@ -102,6 +97,9 @@ class Play extends Phaser.Scene {
             this.clouds.tilePositionX += 0.5;
             this.belt.tilePositionX += game.settings.spaceshipSpeed;
             this.p1Rocket.update();
+            if (game.settings.mode == true) {
+                this.p2Rocket.update()
+            }
             this.ship01.update();
             this.ship02.update();
             this.ship03.update();
