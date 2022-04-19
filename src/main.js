abacd05f8ed7b5ea67257dd816ddd0f9a227784d
shinsoo <elgkim@ucsc.edu>
2022-04-19 00:33:28 -0700

    comment

--- src/main.js
+++ src/main.js
@@ -11,3 +11,16 @@ let config = {
  let borderPadding = borderUISize / 3;
 
 let mode = false;
+
+
+
+//Ellen Kim 
+//Assignment: Rocket Patrol Mods - Donut Kitty Blaster
+// April 19 2022
+// Took about 10 hours
+
+// Point Breakdown:
+// 60 points: redesign artwork, UI, sounds
+// 30 points: simultaneous two-player mode
+// 10 points: parallax scrolling
+// 5 points: control rocket after launched
